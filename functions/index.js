1ea3be315f42fda8591a62fcdde3d8426f955a48
r0bl0xN00b69 <juliussiregar14@gmail.com>
2019-06-28 17:21:42 +0700

    first commit

---
+++ functions/index.js
@@ -0,0 +1,50 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const serviceAccount = require("../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://projectape-bfd4b.firebaseio.com"
+});
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello World you Ape!");
+});
+
+exports.getScreams = functions.https.onRequest((request, response) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then(data => {
+      let scream = [];
+      data.forEach(doc => {
+        scream.push(doc.data());
+      });
+      return response.json(scream);
+    })
+    .catch(err => console.error(err));
+});
+
+exports.createScream = functions.https.onRequest((request, response) => {
+  const newScream = {
+    body: request.body.body,
+    user: request.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then(doc => {
+      response.json({ message: `document ${doc.id} created succesfully` });
+    })
+    .catch(err => {
+      response.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
